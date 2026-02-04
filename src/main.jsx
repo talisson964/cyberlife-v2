0356900a9f2369fc0d624e2eabf9f64c72abf545
talisson964 <talissonmendes964@gmail.com>
2026-02-04 02:21:41 -0300

    remoção pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
-import { registerServiceWorker } from './pwa'
-
-// Registrar o service worker para funcionalidades PWA
-registerServiceWorker()
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
